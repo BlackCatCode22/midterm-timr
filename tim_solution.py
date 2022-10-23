9eb19ef3056be1af8ad6c010155c237d7eb0ea45
timislaw <personal.timross@gmail.com>
2022-10-23 15:45:45 -0700

    trying to make it into a function that can push the values into a new dictionary

---
+++ tim_solution.py
@@ -0,0 +1,44 @@
+#initialized accumulator -- this is to keep track of each line of the file that has a keyword
+
+count = 0
+#setting file to variable and opening file
+# setting file to be opened, read and read all lines
+
+f = open('mbox-short.txt', 'r')
+# msgs = dict()
+
+# for line in f:
+#     if (line[0:5] == 'From '):
+#         split_list = line.split()[1]
+#         # print(split_list)
+#         two_parts = split_list.split('@')
+#         prefix = two_parts[0]
+#         domain = two_parts[1]
+#         # print(prefix)
+#         # print(domain)
+#         count = count + 1
+#         if (prefix in msgs):
+#             print(msgs[prefix], ' ', prefix, ' exists as a key in this dictionary')
+#             msgs[prefix] = msgs[prefix] + 1
+#         else:
+#             msgs[prefix] = 1
+
+
+# print(msgs)
+
+# for line in f:
+#     if (line[0:5] == 'From '):
+#         split_list = line.split()[1]
+#         new_dict = {word for word in line.split() if '@' in word}
+#         print(new_dict)
+
+senders = dict()
+
+def organizer_dict():
+    for line in f:
+        if (line[0:5] == 'From '):
+            senders = {word for word in line.split() if '@' in word}
+            print(senders)
+
+
+organizer_dict()
