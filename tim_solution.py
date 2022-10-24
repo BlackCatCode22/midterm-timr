4de975a0a66f5b9c5d424e4e9f7bde93c3b33a08
timislaw <personal.timross@gmail.com>
2022-10-23 17:45:39 -0700

    cleaning up a bit and added new for loop that splits senders domain and prefix but keeps it in one list for now

--- tim_solution.py
+++ tim_solution.py
@@ -1,62 +1,27 @@
-#initialized accumulator -- this is to keep track of each line of the file that has a keyword
+#count will count up amount of senders found, duplicates included
 
 count = 0
 #setting file to variable and opening file
 # setting file to be opened, read and read all lines
 
 f = open('mbox-short.txt', 'r')
-# msgs = dict()
-
-# for line in f:
-#     if (line[0:5] == 'From '):
-#         split_list = line.split()[1]
-#         # print(split_list)
-#         two_parts = split_list.split('@')
-#         prefix = two_parts[0]
-#         domain = two_parts[1]
-#         # print(prefix)
-#         # print(domain)
-#         count = count + 1
-#         if (prefix in msgs):
-#             print(msgs[prefix], ' ', prefix, ' exists as a key in this dictionary')
-#             msgs[prefix] = msgs[prefix] + 1
-#         else:
-#             msgs[prefix] = 1
-
-
-# print(msgs)
-
-# for line in f:
-#     if (line[0:5] == 'From '):
-#         split_list = line.split()[1]
-#         new_dict = {word for word in line.split() if '@' in word}
-#         print(new_dict)
-
-# senders = dict()
-
-# def organizer_dict(f):
-#     for line in f:
-#         if (line[0:5] == 'From '):
-#             senders = {word for word in line.split() if '@' in word}
-#             # print(senders)
-#             two_fer = senders.split('@')
-#             pref = two_fer[0]
-#             dom = two_fer[1]
-#             print(pref, '\n', dom)
-
-
-# organizer_dict(f)
 
 sender_values = []
 
 for line in f:
     if (line[0:5] == 'From '):
         sender_values.extend(word for word in line.split() if '@' in word)
-        # print(sender_values)
         count = count + 1
 print(count, 'is amount of gathered contacts, dupes included')
-print(sender_values)
 senders = [*set(sender_values)]
-print(senders)
 # *set() will find and remove duplicates in list and return as new list
 
+# See about mapping to organize list into further detailed list
+# find way to implement find method ex: locater = senders[1].find("@") or maybe just split
+pref = []
+dom = []
+two_parts = []
+for i in range(len(senders)):
+    two_parts.extend(senders[i - 1].split('@'))
+
+print(two_parts)
