58d4ba140ef1909b1afef22a64402a78e82bf3ad
timislaw <personal.timross@gmail.com>
2022-10-23 18:36:31 -0700

    made dictionaries. searched online best way to turn list into a dictionary and found a solution, but switched it up so that each key would also be the name of the value. going to change it so that it makes key value equal to 1 and iterates by 1

--- tim_solution.py
+++ tim_solution.py
@@ -12,8 +12,9 @@ for line in f:
     if (line[0:5] == 'From '):
         sender_values.extend(word for word in line.split() if '@' in word)
         count = count + 1
-print(count, 'is amount of gathered contacts, dupes included')
+# print(count, 'is amount of gathered contacts, dupes included')
 senders = [*set(sender_values)]
+# print(senders)
 # *set() will find and remove duplicates in list and return as new list
 
 # See about mapping to organize list into further detailed list
@@ -24,4 +25,23 @@ two_parts = []
 for i in range(len(senders)):
     two_parts.extend(senders[i - 1].split('@'))
 
-print(two_parts)
+# print(two_parts)
+
+dom = two_parts[1::2]
+pref = two_parts[0::2]
+dom = [*set(dom)]
+# print(dom)
+# print(pref)
+
+pref_dict = {}
+dom_dict = {}
+#convert list to dictionary using zip method
+def dict_maker(x, y):
+    it = iter(x)
+    y = dict(zip(it, x))
+    print(y)
+    return y
+
+dom = dict_maker(dom, dom_dict)
+pref = dict_maker(pref, pref_dict)
+print(dom,'\n', pref)
